b41609b74cff02a813d141894ee902d0132fb490
Atilio Pereira <apereira@consumeraffairs.com>
2024-01-09 14:07:26 -0300

    FE-3 Add modal to create Events

---
+++ src/components/AddEventModal.tsx
@@ -0,0 +1,113 @@
+import { Dispatch, SetStateAction } from "react";
+import { Box, Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, TextField } from "@mui/material";
+import React from "react";
+
+
+interface EventFormData {
+    title: string
+    description: string
+    location: string
+    start: Date
+    end: Date
+    tags: string[]
+}
+
+interface DatePickerEventFormData {
+    start: Date
+    end: Date
+}
+
+type FetchEventsCallback = () => void;
+
+interface IProps {
+    open: boolean
+    handleClose: Dispatch<SetStateAction<void>>
+    fetchEvents: FetchEventsCallback
+}
+
+const AddEventModal = ({ open, handleClose, fetchEvents }: IProps) => {
+    const [eventFormData, setEventFormData] = React.useState<EventFormData>({
+        title: "",
+        description: "",
+        location: "",
+        start: new Date(),
+        end: new Date(),
+        tags: [],
+    });
+
+    const onClose = () => handleClose()
+
+    const onChange = (e: { target: { id: any; value: any; }; }) => {
+        setEventFormData({ ...eventFormData, [e.target.id]: e.target.value });
+    }
+
+    const onAddEvent = () => {
+        fetch("http://localhost:8000/api/v1/events/", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(eventFormData),
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                setEventFormData({
+                    title: "",
+                    description: "",
+                    location: "",
+                    start: new Date(),
+                    end: new Date(),
+                    tags: [],
+                });
+                handleClose();
+            })
+            .then(() => fetchEvents())
+            .catch(err => console.log(err));
+    }
+
+    return (
+        <Dialog open={open} onClose={onClose}>
+            <DialogTitle>Add Event</DialogTitle>
+            <DialogContent>
+                <DialogContentText> Please fill out the form below to add an event to the calendar.</DialogContentText>
+                    <Box component="form">
+                        <TextField
+                            autoFocus
+                            margin="dense"
+                            id="title"
+                            label="Title"
+                            type="text"
+                            fullWidth
+                            value={eventFormData.title}
+                            onChange={onChange}
+                        />
+                        <TextField
+                            margin="dense"
+                            id="description"
+                            label="Description"
+                            type="text"
+                            fullWidth
+                            value={eventFormData.description}
+                            onChange={onChange}
+                        />
+                        <TextField
+                            margin="dense"
+                            id="location"
+                            label="Location"
+                            type="text"
+                            fullWidth
+                            value={eventFormData.location}
+                            onChange={onChange}
+                        />
+                    </Box>
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={onClose}>Cancel</Button>
+                <Button onClick={onAddEvent}>Add</Button>
+            </DialogActions>
+        </Dialog>
+    )
+}
+
+export default AddEventModal;
--- src/components/EventCalendar.tsx
+++ src/components/EventCalendar.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Calendar, type Event, dateFnsLocalizer } from "react-big-calendar"
 
-
 import format from "date-fns/format"
 import parse from "date-fns/parse"
 import startOfWeek from "date-fns/startOfWeek"
@@ -12,6 +11,9 @@ import "react-big-calendar/lib/css/react-big-calendar.css"
 
 import EventInfoModal from './EventInfoModal';
 import { set } from 'date-fns';
+import { Box, Button, ButtonGroup, Card, CardContent, CardHeader, Container, Divider } from '@mui/material';
+import { Add } from '@mui/icons-material';
+import AddEventModal from './AddEventModal';
 
 const locales = {
   "en-US": enUS,
@@ -35,9 +37,11 @@ export interface IEventInfo extends Event {
 }
 
 
+
 function EventCalendar() {
     const [currentEvent, setCurrentEvent] = React.useState<IEventInfo | null>(null);
     const [eventInfoModal, setEventInfoModal] = React.useState<boolean>(false);
+    const [addEventModal, setAddEventModal] = React.useState<boolean>(false);
     const [events, setEvents] = React.useState<IEventInfo[]>([]);
 
     const handleSelectEvent = (event: IEventInfo) => {
@@ -45,38 +49,67 @@ function EventCalendar() {
         setEventInfoModal(true);
     }
 
-    React.useEffect(() => {
+    const fetchEvents = () => {
         fetch("http://localhost:8000/api/v1/events/")
-            .then(res => res.json())
-            .then(data => {
-                data.forEach((event: IEventInfo) => {
-                    event.start = new Date(event.start);
-                    event.end = new Date(event.end);
-                });
-                console.log(data);
-                setEvents(data);
+        .then(res => res.json())
+        .then(data => {
+            data.forEach((event: IEventInfo) => {
+                event.start = new Date(event.start);
+                event.end = new Date(event.end);
             });
-    }, []);
+            console.log(data);
+            setEvents(data);
+        });
+    }
 
+
+    React.useEffect(fetchEvents, []);
+    
     return (
-        <div> 
-            <EventInfoModal
-                open={eventInfoModal}
-                handleClose={() => setEventInfoModal(false)}
-                currentEvent={currentEvent as IEventInfo}
-            />
-            <Calendar
-            localizer={localizer}
-            events={events}
-            onSelectEvent={handleSelectEvent}
-            startAccessor="start"
-            endAccessor="end"
-            defaultView="week"
-            style={{
-                height: 900,
+        <Box 
+            mt={2}
+            mb={2}
+            component="main"
+            sx={{
+                flexGrow: 1,
             }}
-            />
-        </div>
+        >
+            <Container maxWidth={false}>
+                <Card>
+                <CardHeader title="WTD (what to do?)" subheader="Create Events and share them easily" />
+                    <CardContent>
+                        <Box sx={{ justifyContent: "space-between" }}>
+                            <ButtonGroup size="large" variant="contained" aria-label="outlined primary button group">
+                                <Button color="secondary">Map View</Button>
+                                <Button color="primary" onClick={() => setAddEventModal(true)}>Create Event</Button>
+                            </ButtonGroup>
+                        </Box>
+                        <Divider style={{ margin: 15 }} />
+                        <EventInfoModal
+                            open={eventInfoModal}
+                            handleClose={() => setEventInfoModal(false)}
+                            currentEvent={currentEvent as IEventInfo}
+                        />
+                        <AddEventModal
+                            open={addEventModal}
+                            handleClose={() => setAddEventModal(false)}
+                            fetchEvents={fetchEvents}
+                        /> 
+                        <Calendar
+                            localizer={localizer}
+                            events={events}
+                            onSelectEvent={handleSelectEvent}
+                            startAccessor="start"
+                            endAccessor="end"
+                            defaultView="week"
+                            style={{
+                                height: 900,
+                            }}
+                        />
+                    </CardContent>
+                </Card>
+            </Container>
+        </Box>
         );
   }
   
